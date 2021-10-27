df9b3772c575eb969b7e1d513287a8971ea3148e
thastings <thasting@uccs.edu>
2021-10-27 01:10:43 +0000

    init

---
+++ spec/controller/projects_spec.rb
@@ -0,0 +1,19 @@
+require "rails_helper"
+
+RSpec.describe ProjectsController, type: :controller do
+    context "GET #index" do
+        it "returns a success response" do
+            get :index
+
+            expect(response).to be_success
+        end
+    end
+
+    context "GET #show" do
+        let!(:project) { Project.create(name: "Test name", description: "Test description") }
+        it "returns a success response" do
+            get :show, params: { id: project }
+            expect(response).to be_success
+        end
+    end
+end
